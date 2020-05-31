b1e0d62ed9e6d645ae9e9bcd9557b66048324898
Victor Kernes Jr <victorkernes@gmail.com>
2020-05-31 09:40:15 -0700

    Adding site analytics

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,7 +1,9 @@
 import Nav from "../Nav";
+import SEO from "../SEO";
 
 export default () => (
   <React.Fragment>
+    <SEO />
     <Nav />
   </React.Fragment>
 );
---
+++ components/SEO/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function SEO() {
+  return (
+    <React.Fragment>
+      <Head>
+        <script>
+          var clicky_site_ids = clicky_site_ids || [];
+          clicky_site_ids.push(101217952);
+        </script>
+        <script async src="//static.getclicky.com/js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Victor Kernes – Product Designer</title>
+      </Head>
+    </React.Fragment>
+  );
+}
