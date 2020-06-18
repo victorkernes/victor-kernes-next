66f7cd9a5f97999a58e89cdf627cf9d1f157382e
Victor Kernes Jr <victorkernes@gmail.com>
2020-06-17 23:02:52 -0700

    Change favicon

--- components/SEO/index.js
+++ components/SEO/index.js
@@ -10,6 +10,27 @@ export default function SEO() {
         </script>
         <script async src="//static.getclicky.com/js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="../../static/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="../../static/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="../../static/favicon/favicon-16x16.png"
+        />
+        <link
+          rel="manifest"
+          href="../../static/favicon/site.webmanifest"
+        ></link>
         <title>Victor Kernes – Product Designer</title>
       </Head>
     </React.Fragment>
