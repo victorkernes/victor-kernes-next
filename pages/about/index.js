97012f435a4f5f9bd6943a965f4a53263631442c
Victor Kernes Jr <victorkernes@gmail.com>
2021-02-09 21:28:21 -0800

    Update About page

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,17 +3,17 @@ import Page from "../../components/Page";
 export default () => (
   <Page>
     <section className={"AboutMeSection"}>
-      <h1 className={"f-headline AboutMeDescription"}>About Me</h1>
-      <h2 className={"f2 AboutMeContext"}>
-        I’m a self-taught User Experience (UX) & Product Designer with over 5
-        years of experience. I have a background in marketing, education, and
+      <h1 className={"Title"}>About</h1>
+      <p className={"Context"}>
+        I’m a digital Product Designer with over five
+        years of experience. I’ve worked in industries such as marketing, education, and
         healthcare. In my
         free time, you’ll find me playing basketball at the YMCA, visiting
         coffee shops, and hanging with my family.
-      </h2>
-      <h2 className={"f2 AboutMeContext"}>
+      </p>
+      <p className={"Context"}>
         If you would like to get in touch, feel free to <a href="mailto:victorkernes@gmail.com">email me</a>.
-      </h2>
+      </p>
     </section>
   </Page>
 );
