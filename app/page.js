58c073f045a815f3b486b0f418c8ca9349d89edc
Victor Kernes Jr <victorkernes@Victors-MacBook-Pro-2.local>
2022-10-26 06:42:35 -0700

    Upgrade to Next.js 13 and add /app directory

---
+++ app/page.js
@@ -0,0 +1,10 @@
+// app/page.js
+// This file maps to the index route (/)
+export default function Page() {
+    return( 
+        <>
+            <Link href="/blog">Blog</Link>
+            <h1>Hello, Next.js!</h1>
+        </>      
+    );
+  }
